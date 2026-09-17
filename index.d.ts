f443260bb4687eea3ac24bde70d3c86c36a8fc1c
Seal <info@sealsecurity.io>
2026-09-17 17:26:42 +0000

    CVE-2025-27152

--- index.d.ts
+++ index.d.ts
@@ -76,6 +76,7 @@ export interface AxiosRequestConfig<D = any> {
   url?: string;
   method?: Method | string;
   baseURL?: string;
+  allowAbsoluteUrls?: boolean;
   transformRequest?: AxiosRequestTransformer | AxiosRequestTransformer[];
   transformResponse?: AxiosResponseTransformer | AxiosResponseTransformer[];
   headers?: AxiosRequestHeaders;
--- test/specs/options.spec.js
+++ test/specs/options.spec.js
@@ -81,6 +81,21 @@ describe('options', function () {
       done();
     });
   });
+  
+  it('should combine the URLs if base url and request url exist and allowAbsoluteUrls is false', function (done) {
+    const instance = axios.create({
+      baseURL: 'http://someurl.com/',
+      allowAbsoluteUrls: false
+    });
+
+    instance.get('http://someotherurl.com/');
+
+    getAjaxRequest().then(function (request) {
+      expect(request.url).toBe('http://someurl.com/http://someotherurl.com/');
+      done();
+    });
+
+  });
 
   it('should change only the baseURL of the specified instance', function() {
     var instance1 = axios.create();
